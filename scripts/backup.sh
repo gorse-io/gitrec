5a9e62e3f3a6fa8cf6bd76eb0f48cfef18743343
zhenghaoz <zhangzhenghao@hotmail.com>
2023-04-24 13:09:28 +0000

    disable MySQL binlog

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -5,7 +5,7 @@ while true; do
     SQL_FILE=$(date '+%Y-%m-%d.%H').sql
 
     # Dump database
-    mysqldump --no-tablespaces -h ${MYSQL_HOST:=127.0.0.1} -u ${MYSQL_USER:=gorse} -p${MYSQL_PASSWORD:=gorse_pass} ${MYSQL_DATABASE:=gorse} > $SQL_FILE
+    mysqldump --no-tablespaces -h ${MYSQL_HOST:=127.0.0.1} -u ${MYSQL_USER:=gorse} -p${MYSQL_PASSWORD:=gorse_pass} ${MYSQL_DATABASE:=gorse} users items feedback flask_dance_oauth > $SQL_FILE
 
     # Upload SQL file
     s3cmd --access_key=$S3_ACCESS_KEY \
@@ -13,7 +13,7 @@ while true; do
         --region=$S3_BUCKET_LOCATION \
         --host=$S3_HOST_BASE \
         --host-bucket=$S3_HOST_BUCKET \
-        put $SQL_FILE s3://${S3_BUCKET:=gorse.cloud}${S3_PREFIX:=/gitrec}/$SQL_FILE
+        put $SQL_FILE s3://${S3_BUCKET}${S3_PREFIX}/$SQL_FILE
 
     # Backup 1 day later.
     sleep 86400
