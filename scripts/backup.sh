a6b6fab864954a69e4f501ee855019cdfb0fd59d
zhenghaoz <zhangzhenghao@hotmail.com>
2025-08-17 08:19:41 +0800

    Fix "TLS/SSL error: self-signed certificate in certificate chain"

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -5,7 +5,7 @@ while true; do
     SQL_FILE=$(date '+%Y-%m-%d.%H').sql
 
     # Dump database
-    mysqldump --no-tablespaces -h ${MYSQL_HOST:=127.0.0.1} -u ${MYSQL_USER:=gorse} -p${MYSQL_PASSWORD:=gorse_pass} ${MYSQL_DATABASE:=gorse} users items feedback flask_dance_oauth > $SQL_FILE
+    mysqldump --no-tablespaces -h ${MYSQL_HOST:=127.0.0.1} -u ${MYSQL_USER:=gorse} -p${MYSQL_PASSWORD:=gorse_pass} --ssl-verify-server-cert=0 ${MYSQL_DATABASE:=gorse} users items feedback flask_dance_oauth > $SQL_FILE
 
     # Upload SQL file
     s3cmd --access_key=$S3_ACCESS_KEY \
