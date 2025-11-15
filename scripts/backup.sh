9aee27fee93beac031d29e51df3a8afde246a7a0
zhenghaoz <zhangzhenghao@hotmail.com>
2025-11-15 12:21:32 +0000

    Compress backup SQL file

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -7,13 +7,16 @@ while true; do
     # Dump database
     mysqldump --no-tablespaces -h ${MYSQL_HOST:=127.0.0.1} -u ${MYSQL_USER:=gorse} -p${MYSQL_PASSWORD:=gorse_pass} --ssl-verify-server-cert=0 ${MYSQL_DATABASE:=gorse} users items feedback flask_dance_oauth > $SQL_FILE
 
+    # Compress SQL file
+    gzip $SQL_FILE
+
     # Upload SQL file
     s3cmd --access_key=$S3_ACCESS_KEY \
         --secret_key=$S3_SECRET_KEY \
         --region=$S3_BUCKET_LOCATION \
         --host=$S3_HOST_BASE \
         --host-bucket=$S3_HOST_BUCKET \
-        put $SQL_FILE s3://${S3_BUCKET}${S3_PREFIX}/$SQL_FILE
+        put ${SQL_FILE}.gz s3://${S3_BUCKET}${S3_PREFIX}/${SQL_FILE}.gz
 
     # Backup 1 day later.
     sleep 86400
