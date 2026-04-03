c015fd83c61d5500298f83b0dcef8ff26b4385ff
Zhenghao Zhang <33406104+zhangzhenghao@users.noreply.github.com>
2026-04-03 09:41:50 +0800

    Migrate from Vue 2 to Vue 3 (#144)

--- frontend/src/login/main.js
+++ frontend/src/login/main.js
@@ -1,12 +1,9 @@
-import Vue from "vue";
+import { createApp } from "vue";
 import App from "./App.vue";
 import "@materializecss/materialize/dist/css/materialize.min.css";
 import "github-markdown-css/github-markdown.css";
-import 'material-design-icons-iconfont/dist/material-design-icons.css'
+import 'material-design-icons-iconfont/dist/material-design-icons.css';
 import "@fortawesome/fontawesome-free/css/all.css";
 
-Vue.config.productionTip = false;
-
-new Vue({
-  render: (h) => h(App),
-}).$mount("#app");
+const app = createApp(App);
+app.mount("#app");
